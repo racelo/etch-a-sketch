7a77dc39a59ddfd790e60a2fb581ea154a0e61a5
racelo <kraef@protonmail.com>
2024-08-29 17:58:22 +0800

    Use the replaceChildren() method after the variale declarations to clear out the previous div.square elements before adding the new set of div elements

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-const container = document.querySelector(".container");
-let divSquare = "";
+const parent = document.querySelector(".container");
 
-for (let i = 0; i < 100; i++) {
-  divSquare = document.createElement("div");
-  divSquare.className = "square";
-  // divSquare.setAttribute("class", "square");
-  // divSquare.classList.add("square");
-  container.appendChild(divSquare);
-}
+const gridBtn = document.querySelector(".grid-size");
+const gridValue = document.querySelector("#grid-value");
+const gridInput = document.querySelector("#grid-input");
 
-const squares = document.querySelectorAll("div.square");
-
-// squares.forEach((square) => {
-//   square.addEventListener("click", () => {
-//     square.style.backgroundColor = "green";
-//   });
-// });
+gridInput.addEventListener("change", (event) => {
+  gridValue.textContent = event.target.value;
+});
 
 function randomColor() {
   let red = Math.floor(Math.random() * 256);
@@ -24,10 +15,38 @@ function randomColor() {
   return `rgb(${red}, ${green}, ${blue})`;
 }
 
-squares.forEach((square) => {
-  square.addEventListener("mouseover", () => {
-    // square.style.backgroundColor = "green";
-    let color = randomColor();
-    square.style.backgroundColor = color;
+gridInput.addEventListener("input", (e) => {
+  let inputValue = parseInt(e.target.value);
+  let gridNum = inputValue * inputValue;
+  let gridSize = 500 / inputValue;
+
+  // Alternative methods of emptying the parent container
+  // parent.innerHTML = "";
+  // while (parent.firstChild) {
+  //   parent.removeChild(parent.firstChild);
+  // }
+
+  // remove() was introduced in 2011
+  // while (parent.firstChild) {
+  //   parent.firstChild.remove();
+  // }
+
+  // replaceChildren was introduced in 2021
+  parent.replaceChildren();
+
+  for (let i = 1; i <= gridNum; i++) {
+    let divSquare = document.createElement("div");
+    divSquare.className = "square";
+    divSquare.style.width = `${gridSize}px`;
+    divSquare.style.height = `${gridSize}px`;
+    parent.appendChild(divSquare);
+  }
+
+  const squares = document.querySelectorAll("div.square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      let color = randomColor();
+      square.style.backgroundColor = color;
+    });
   });
 });
